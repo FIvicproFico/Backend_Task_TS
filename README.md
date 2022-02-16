31c04bcc4febe86d0fa4ac2f1f31e6b2bddd08e5
FIvicproFico <fivic@profico.hr>
2022-02-16 12:52:12 +0100

    Project initialization

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# express-boilerplate
-express-boilerplate
+# Backend_Task_TS
+
+```
+$ npm run "script"
+```
+
+## MySQL
+
+```
+$ mysql.server start
+$ mysql.server stop
+$ mysql -u root -p
+```
+
+## Sequelize
+
+```
+//Project bootstrapping
+$ npx sequelize-cli init
+
+```
--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -1,10 +1,25 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 const router = express.Router();
+dotenv.config();
 
-/* GET users listing. */
+interface IUser {
+  username: string;
+  email: number;
+  password: string;
+}
+
+/* GET login. */
 router.get('/', (req: express.Request, res: express.Response): void => {
   res.send('Login!');
+});
+
+/* POST data to login. */
+router.post('/', (req: express.Request, res: express.Response): void => {
+  const { username, email, password }: IUser = req.body;
+  const env: string = process.env.DB_USERNAME;
+  res.json({ username, email, password, env });
 });
 
 export { router as loginRouter };
